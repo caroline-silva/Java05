424ecfdbd170c1b60bc7c10ece68dc975c366ab6
ne_aps <58328252+caroline-silva@users.noreply.github.com>
2020-08-28 22:42:23 -0300

    Add files via upload

---
+++ array.java
@@ -0,0 +1,14 @@
+public class array {
+	
+	public static void main(String[] args) {
+		String[] carros = {"Volvo", "BMW", "Ford", "Mazda"}; //iniciando array string
+		int [] numeros = {3, 5 , 20, 76, 82, 53}; //inicinado array numeros
+		System.out.println(carros[3]); //aparece apenas dado de um indice do array
+		System.out.println("tamanho carros = " + carros.length); //tamanho do array
+		
+		System.out.println(numeros[3]); //aparece apenas dado de um indice do array
+		System.out.println("tamanho numeros = " + numeros.length); //tamanho do array
+		
+	}
+
+}
